6a32df42eb8ae6012affeee91169b2b1256056db
jsongkim <josephms2022@gmail.com>
2025-03-11 16:47:28 -0700

    Updated with comments.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include "studentType.h"
 #include "professorType.h"
 
+// Notes for David and Kyle:
+// The object creation is meant for the A11 assignment. If you are using this code for A8, A9, etc., it asks you to create different objects.
+// Check the homework guidelines if you are going to use this code. 
+
 int main(){
 
 	 // Declaring one person
@@ -15,26 +19,26 @@ int main(){
 	 professorType professor1("Haley", "River", "456 Cardinal St", 70.5, "09/09/2009", 'F', "0001", "Mathematics", "Electrical Engineering");
 	 professorType professor2("Hailey", "Stream", "789 Pope St", 71.5, "08/08/2008", 'M', "0002", "Computer Science", "Aerospace Engineering");
 
-    // Printing all five individuals
-    cout << "Printing all five people:" << endl;
-    person1.print();
-	 cout << endl;
-	 student1.print();
-	 cout << endl;
-	 student2.print();
-	 cout << endl;
-	 professor1.print();
-	 cout << endl;
-	 professor2.print();
-	 cout << endl;
+    	// Printing all five individuals
+    	cout << "Printing all five people:" << endl;
+    	person1.print();
+	cout << endl;
+	student1.print();
+	cout << endl;
+	student2.print();
+	cout << endl;
+	professor1.print();
+	cout << endl;
+	professor2.print();
+	cout << endl;
 
-
-	 if(professor1.equals(professor2) == true){
-	 	cout << "The two professors are the same." << endl;
-	 }
-	 else{
-	 	cout << "The two professors are not the same." << endl;
-	 }
+	// Checks  if the objects are equal.
+	if(professor1.equals(professor2) == true){
+		cout << "The two professors are the same." << endl;
+	}
+	else{
+		cout << "The two professors are not the same." << endl;
+	}
 
     return 0;
 
